7401229c4d3619a5b5eb8e436b6f2986760426cf
HanHuili <80623987+Hanhuilii@users.noreply.github.com>
2021-04-21 20:49:02 +0800

    Create CloudHttpServer.java

---
+++ week1 Optional HW1/CloudHttpServer.java
@@ -0,0 +1,21 @@
+public class CloudHttpServer {
+
+ /**
+
+ * 启动服务器
+
+ */
+
+ public static void startServer() {
+
+ NioServer.getServerInstance().start();
+
+ }
+
+ public static void main(String[] args) {
+
+ startServer();
+
+ }
+
+}
