e905a5408443d1450a6c78afc60a281f17040b1a
HanHuili <80623987+Hanhuilii@users.noreply.github.com>
2021-04-21 20:28:08 +0800

    Create Request.java

---
+++ week1 Optional HW1/Request.java
@@ -0,0 +1,51 @@
+public class Request {
+
+ /**
+
+ * method
+
+ */
+
+ private String method;
+
+ /**
+
+ * http协议版本
+
+ */
+
+ private String httpVersion;
+
+ /**
+
+ * 请求uri
+
+ */
+
+ private String uri;
+
+ /**
+
+ * 请求相对路径
+
+ */
+
+ private String path;
+
+ /**
+
+ * 请求头信息
+
+ */
+
+ private Map<String, String> headers;
+
+ /**
+
+ * 请求参数
+
+ */
+
+ private Map<String, String> attribute;
+
+}
