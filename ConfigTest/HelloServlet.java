5a6053f8d5f9c7f9f3c5a8fb24c690e5613ff078
HanHuili <80623987+Hanhuilii@users.noreply.github.com>
2021-04-22 08:48:19 +0800

    Create HelloServlet.java

---
+++ ConfigTest/HelloServlet.java
@@ -0,0 +1,35 @@
+Servlet
+
+package com.example.ConfigTest;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        out.println("test");
+        java.util.Enumeration e=getServletConfig().getInitParameterNames();
+        out.println("<br>param name="+e.nextElement()+"<br>");
+        out.println("main email is"+getServletConfig().getInitParameter("mainEmail"));
+        out.println("admin email is"+getServletConfig().getInitParameter("adminEmail"));
+        // Hello
+
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
+
